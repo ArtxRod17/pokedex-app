0c3767f80fe6c3ed9573b51ba7243eab1b96ab1d
Arturo <chiefchurro@gmail.com>
2023-06-20 20:38:56 -0700

    made cards smaller

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -2,14 +2,16 @@ const PokemonCard = ({ name, url, number }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 m-4 bg-gray-500 hover:bg-red-500">
       <img
+        className=""
         src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${number}.png`}
+        width={150}
+        height={150}
         alt=""
       />
       <div className="px-6 py-4">
-        <div className=" font-bold text-3xl mb-2 flex justify-center">
+        <div className=" font-bold text-2xl mb-2 flex justify-center">
           {name}
         </div>
-        <p>{url}</p>
       </div>
     </div>
   );
